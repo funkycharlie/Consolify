84ea2aa2c552f961e2fbf6f798c12f855710fa7e
Charlie <152520435+funkycharlie@users.noreply.github.com>
2023-11-30 23:10:36 +0000

    Update API keys

--- consolify_pkgs/sp_module.py
+++ consolify_pkgs/sp_module.py
@@ -1,10 +1,14 @@
 import spotipy
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
+import os
+
+client_id = os.environ['API']
+client_secret = os.environ['API_SECRET']
 scope = "user-read-playback-state,user-modify-playback-state"
 sp: Spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,
-                                               client_id='e1cda6ac7c13441d8302de7b313b97ea',
-                                               client_secret='7ccac525673e455c9778614487dcf453',
+                                               client_id=client_id,
+                                               client_secret=client_secret,
                                                redirect_uri='http://localhost:8080',
                                                cache_path='cache.cache'
                                                ))
