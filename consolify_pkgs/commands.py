b9bcc751c96a4bd6473e2bb2c92a0e19992fea1a
funkycharlie <funkycharlie4@gmail.com>
2023-11-30 17:15:38 +0000

    Added search function

--- consolify_pkgs/commands.py
+++ consolify_pkgs/commands.py
@@ -3,9 +3,23 @@ from .sp_module import sp
 
 def nowplaying():
     current_song = sp.current_playback()
-    if current_song and 'item' in current_song and 'name' in current_song['item']:
+    if current_song['item'] is not None:
         song_name = current_song['item']['name']
         artists = ', '.join([artist['name'] for artist in current_song['item']['artists']])
         print(f"Now playing: {song_name} by {artists}.")
     else:
         print("No song currently playing.")
+
+
+def spotify_search():
+    while True:
+        search_str = input("Consolify/Search >")
+        if search_str == "back":
+            break
+        result = sp.search(q=search_str, limit=20)
+        for i, t in enumerate(result['tracks']['items']):
+            track_name = t['name']
+            artists = ', '.join([artist['name'] for artist in t['artists']])
+            print(f" {i} {track_name} by {artists}")
+
+
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from consolify_pkgs import commands
 
 def main():
     while True:
-        user_input = input(">")
+        user_input = input("Consolify >")
         if user_input == "nowplaying":
             commands.nowplaying()
+        elif user_input == "search":
+            commands.spotify_search()
         else:
             print("Not a command! Type 'help' for a list of commands.")
 
