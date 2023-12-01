d4dc9ddb8ea83807f3e10de5529c999094607268
funkycharlie <funkycharlie4@gmail.com>
2023-12-01 15:38:02 +0000

    Updated the way API keys are handled.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from consolify_pkgs import commands
+import spotipy
+from consolify_pkgs.sp_module import sp
 
 
 def main():
+    try:
+        user_profile = sp.current_user()
+        print("Successfully authenticated. User profile:")
+        print(f"Display name: {user_profile['display_name']}")
+        print(f"User ID: {user_profile['id']}")
+
+    except spotipy.SpotifyException as e:
+        print(f"Error accessing Spotify API: {e}")
     while True:
+
         user_input = input("Consolify >")
         if user_input == "nowplaying":
             commands.nowplaying()
@@ -16,4 +27,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
