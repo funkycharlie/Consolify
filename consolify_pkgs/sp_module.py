4c7226730f2f21b0e1dd84a01337cbc619bf354a
funkycharlie <funkycharlie4@gmail.com>
2023-12-01 15:38:02 +0000

    Updated the way API keys are handled.

--- consolify_pkgs/sp_module.py
+++ consolify_pkgs/sp_module.py
@@ -1,14 +1,61 @@
-import spotipy
-from spotipy import Spotify
-from spotipy.oauth2 import SpotifyOAuth
+import json
 import os
 
-client_id = os.environ['API']
-client_secret = os.environ['API_SECRET']
-scope = "user-read-playback-state,user-modify-playback-state"
-sp: Spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,
-                                               client_id=client_id,
-                                               client_secret=client_secret,
-                                               redirect_uri='http://localhost:8080',
-                                               cache_path='cache.cache'
-                                               ))
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+CONFIG_FILE = 'config.json'
+CACHE_FILE = 'cache.cache'
+REDIRECT_URI = 'http://localhost:8080'
+SCOPE = 'user-read-playback-state,user-modify-playback-state'
+
+
+def get_spotify_credentials():
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, 'r') as file:
+            config = json.load(file)
+            client_id = config.get('client_id')
+            client_secret = config.get('client_secret')
+            return client_id, client_secret
+    else:
+        return None, None
+
+
+def save_spotify_credentials(client_id, client_secret):
+    with open(CONFIG_FILE, 'w') as file:
+        json.dump({'client_id': client_id, 'client_secret': client_secret}, file)
+
+
+def authenticate_spotify():
+    client_id, client_secret = get_spotify_credentials()
+
+    if not client_id or not client_secret:
+        client_id = input("Consolify/Client ID >")
+        client_secret = input("Consolify/Client Secret >")
+        save_spotify_credentials(client_id, client_secret)
+
+    try:
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE,
+                                                       client_id=client_id,
+                                                       client_secret=client_secret,
+                                                       redirect_uri=REDIRECT_URI,
+                                                       cache_path=CACHE_FILE))
+        return sp
+    except Exception as e:
+        print(f"Error during authentication: {e}")
+        return None
+
+
+try:
+    sp = authenticate_spotify()
+
+    if sp:
+        # Now you can use sp for your Spotify API requests
+        print("Authentication successful!")
+        # Example: Print the user's display name
+        user_profile = sp.current_user()
+        display_name = user_profile.get('display_name', 'Unknown')
+        print(f"Welcome, {display_name}!")
+
+except Exception as e:
+    print(f"Error: {e}")
