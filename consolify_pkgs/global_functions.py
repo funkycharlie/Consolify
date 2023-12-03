98854f8f98b867a7b7d80536f5976a4b59e6ab1c
funkycharlie <funkycharlie4@gmail.com>
2023-12-03 20:04:48 +0000

    Added a new create playlist command

--- consolify_pkgs/global_functions.py
+++ consolify_pkgs/global_functions.py
@@ -1,5 +1,5 @@
-from .sp_module import sp
 import spotipy
+from .sp_module import sp
 
 
 def choose_device():
@@ -78,4 +78,98 @@ def add_to_queue(selected_track):
         print(f"There was an error adding the track to the queue: {e}")
 
 
+def spotify_search_global(args):
+    def play_search(check_quick):
+        try:
+            track_number_int = 0 if check_quick else int(input("Consolify/Search/Play > "))
+            selected_track = result['tracks']['items'][track_number_int]
+            if track_number_int == "back":
+                return "back"
+            playback(selected_track)
+        except (ValueError, IndexError):
+            print("Invalid input. Please enter a valid track number or type 'back' to go back.")
+            play_search(check_quick)
 
+    def add_queue_search(check_quick):
+        try:
+            track_number = 0 if check_quick else int(input("Consolify/Search/Add To Queue > "))
+            selected_track = result['tracks']['items'][track_number]
+            if track_number == "back":
+                return "back"
+            add_to_queue(selected_track)
+        except (ValueError, IndexError):
+            print("Invalid input. Please enter a valid track number or type 'back' to go back.")
+            add_queue_search(check_quick)
+
+    while True:
+        search_str = input("Consolify/Search > ")
+        if search_str == "back":
+            break
+
+        try:
+            result = sp.search(q=search_str, limit=10)
+            print(" ")
+
+            if "-p" and "-q" not in args:
+                for i, t in enumerate(result['tracks']['items']):
+                    track_name = t['name']
+                    artists = ', '.join([artist['name'] for artist in t['artists']])
+                    album = t['album']['name']
+                    print(f" {i} {track_name} by {artists}")
+                    if "-a" in args:
+                        print(f"Album: {album}")
+                    print(" ")
+
+            if "-p" in args:
+                play_search_result = play_search(True)
+                if play_search_result == "back":
+                    break
+                continue
+
+            if "-q" in args:
+                add_queue_search_result = add_queue_search(True)
+                if add_queue_search_result == "back":
+                    break
+                continue
+
+        except spotipy.SpotifyException as e:
+            print(f"Error: Search query not valid: {e}")
+            continue
+
+        if "Playlist" in args:
+            while True:
+                user_choice = input("Search/Add to Playlist > ")
+                if user_choice == "back":
+                    return "back"
+                try:
+                    user_choice = int(user_choice)
+                    track = result['tracks']['items'][user_choice]
+                    return track['uri']
+                except (ValueError, IndexError):
+                    print("That's not a valid choice, please try again.")
+                    continue
+
+        print(""" 
+What would you like to do? You could say: 'play', 'back' or 'add queue'
+ """)
+        user_choice = input("Spotify/Search/Action > ")
+        if user_choice == "back":
+            continue
+        elif user_choice == "play":
+            play_search_result = play_search(False)
+            if play_search_result == "back":
+                continue
+        elif user_choice == "add queue":
+            add_queue_search_result = add_queue_search(False)
+            if add_queue_search_result == "back":
+                continue
+        else:
+            print('Invalid input, please try again')
+
+
+def add_songs_playlist(playlist):
+    track_uri = [spotify_search_global(["Playlist"])]
+    if track_uri == "back":
+        return "back"
+    print("Adding tracks to playlist:", track_uri)
+    sp.playlist_add_items(playlist_id=playlist, items=track_uri)
--- consolify_pkgs/sp_module.py
+++ consolify_pkgs/sp_module.py
@@ -7,7 +7,7 @@ from spotipy.oauth2 import SpotifyOAuth
 CONFIG_FILE = 'config.json'
 CACHE_FILE = 'cache.cache'
 REDIRECT_URI = 'http://localhost:8080'
-SCOPE = 'user-read-playback-state,user-modify-playback-state,user-read-recently-played'
+SCOPE = 'user-read-playback-state,user-modify-playback-state,user-read-recently-played,playlist-modify-public,playlist-modify-private'
 
 
 def get_spotify_credentials():
